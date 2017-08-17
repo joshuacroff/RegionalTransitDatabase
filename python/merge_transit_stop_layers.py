d1b97ae5640366989940a15dca6b1b60dd0a01e5
jcroff <joshua.croff@gmail.com>
2017-08-17 10:06:06 -0700

    Update merge transit stop layer code to also populate important fields with content
    Updated system field for bus stops with ‘bus’
    Updated agency name field with agency name based on agency id field

--- python/merge_transit_stop_layers.py
+++ python/merge_transit_stop_layers.py
@@ -1,16 +1,15 @@
 #This script should be run within the ArcGIS Pro Python window
-
 import arcpy 
 
-outfc = r"TPA_Eligible_Transit_Stops_2017"
+outfc = r"\\Mac\Home\Documents\Planning\DataManagement\DataManagement.gdb\TPA_Eligible_Transit_Stops_2017"
 
-# Merge the following two feature classes together - map fields with the same content to single field
-# Each of the FC below were added to the project from MTC Organization ArcGIS Online
+#Merge the following two feature classes together - map fields with the same content to single field
+#Each of the FC below were added to the project from MTC Organization ArcGIS Online
 
 railfc = r"TPA_Non_Bus_Eligible_Stops_2017"
 busfc = r"High_Frequency_Bus_Stops"
 
-# Create a new fieldmappings object and add the two input feature classes
+#Create a new fieldmappings object and add the two input feature classes
 
 fieldmappings = arcpy.FieldMappings()
 fieldmappings.addTable(railfc)
@@ -19,7 +18,7 @@ fieldmappings.addTable(busfc)
 #Get fieldmap for two fields in rail dataset which bus values will be merged into
 #    - Create array of bus fields which are to be mapped (input fields)
 #    - Create array of rail fields which bus fields will be mapped to (destination fields)
-#    - Create dict using zip methold - ensure order of fields matches map - to order within array    
+#    - Create dict using zip methold - ensure order of fields matches map - to order within array 
 
 busfields = ["stop_id","lgcl_adjacent_hf_routes"]
 railfields = ["agency_stop_id","Distance_Eligible"]
@@ -42,10 +41,42 @@ for busfield, railfield in fieldmapdict.items():
 vTab = arcpy.ValueTable()
 vTab.addRow(railfc)
 vTab.addRow(busfc)
-
 #merge 
 
 arcpy.Merge_management(vTab, outfc, fieldmappings)
+
+#Calculate Transit System Field (system) as "bus" for all null values 
+
+fieldName = r"system"
+expression = r"transitSystemType(!system!)"
+codeblock = r"""def transitSystemType(field):
+    if (field is None):
+        return "Bus"
+    else:
+        return field """
+
+arcpy.management.CalculateField(outfc, fieldName, expression, "PYTHON_9.3", codeblock)
+
+#Calculate Agency Name Field (agency_name) using Agency ID for all null values
+
+fieldName = r"agency_name"
+expression = r"agencyName(!agency_id!,!agency_name!)"
+codeblock = r"""def agencyName(id, name):
+    agencynamelookup = {"AC":"AC Transit", "EM":"Emmery Go Around", "SC":"VTA", "SF":"SFMTA", "SM":"SamTrans"}
+    if (name is None):
+        return agencynamelookup[id]
+    else:
+        return name """
+
+arcpy.management.CalculateField(outfc, fieldName, expression, "PYTHON_9.3", codeblock)
+
+
+
+
+
+
+
+
 
 
 
